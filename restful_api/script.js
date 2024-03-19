d5d04eee2413ff3ac770bfc0235687a4cd423af0
deepaksharmadk <dks847203@gmail.com>
2024-03-20 01:03:58 +0530

    completed

--- restful_api/script.js
+++ restful_api/script.js
@@ -1,8 +1,41 @@
 let container = document.querySelector(".counties-container");
-async function fetchDate() {
-  const allCountry = await fetch("https://restcountries.com/v3.1/all");
-  const res = await allCountry.json();
-  res.forEach((element) => {
+const filterByRegion = document.querySelector(".filter-by-region");
+// console.log(filterByRegion);
+const searchInput = document.querySelector(".search-container input");
+let allCountriesData;
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    RenderCountry(data);
+    allCountriesData = data;
+  });
+async function w() {
+  console.log(filterByRegion.value);
+  const isWait = await fetch(
+    `https://restcountries.com/v3.1/region/${filterByRegion.value}`
+  );
+  let doneFecthdata = await isWait.json();
+  await RenderCountry(doneFecthdata);
+}
+
+const countriesContainer = document.querySelector(".countries-container");
+filterByRegion.addEventListener("change", w);
+
+// filterByRegion.addEventListener("change", (e) => {
+//   console.log(e.target.value);
+//   fetch(`https://restcountries.com/v3.1/region/${e.target.value}}`)
+//     .then((res) => res.json())
+//     .then(() => {});
+// });
+async function RenderCountry(data) {
+  container.innerHTML = "";
+  console.log(data);
+  // const allCountry = await fetch("https://restcountries.com/v3.1/all");
+  // const res = await allCountry.json();
+
+  console.log(data);
+  data.forEach((element) => {
     // console.log(element.flags.svg);
     // console.log(element.capital?.[0]);
 
@@ -14,15 +47,22 @@ async function fetchDate() {
   <h3 class="card-title">${element.name.common}</h3>
   <p>Population:<b>${element.population.toLocaleString("en-IN")}</b></p>
   <p>Region: <b>${element.region}</b></p>
-  <p>capital: <b>${element.capital?.[0]}n</b></p>
+  <p>capital: <b>${element.capital?.[0]}</b></p>
 </div>`;
 
     container.append(createAnchor);
   });
 }
-fetchDate();
 // theme chaner
 const themeChanger = document.querySelector(".theme-changer");
 themeChanger.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+searchInput.addEventListener("input", (e) => {
+  let countryByName = allCountriesData.filter((country) =>
+    country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  RenderCountry(countryByName);
+});
+console.log(filterByRegion);
